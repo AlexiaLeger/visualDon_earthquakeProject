117b93e5877929253b012b3dcdec423d16d3e6fb
Marielle Hänggeli <marielle.hanggeli@gmail.com>
2022-03-29 10:43:11 +0200

    Add todo.md

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import * as d3 from 'd3';
 
 //récupération des données
-import data from '../data/data.json';
-//import locationData from '../data/locations.json';
+import dataEarthquakes from '../data/data.json';
+import locationData from '../data/locations.json';
 
 //récupération svg
 
@@ -23,11 +23,14 @@ const path = d3.geoPath().projection(projection);
 const center = [width / 2, height / 2];
 
 drawGlobe();
+console.log("Globe draw");
 drawGraticule();
+console.log("Graticule draw");
 enableRotation();
-console.log(d3.json('../data/locations.json'));
+console.log("Rotation enable");
 
 function drawGlobe() {
+    console.log('test1')
     Promise.all([
         d3.json('https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json'),
         d3.json('../data/locations.json')
@@ -48,6 +51,7 @@ function drawGlobe() {
 }
 
 function drawGraticule() {
+    console.log('test2')
     const graticule = d3.geoGraticule()
         .step([10, 10]);
 
@@ -60,6 +64,7 @@ function drawGraticule() {
 }
 
 function enableRotation() {
+    console.log('test3')
     d3.timer(function (elapsed) {
         projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
         svg.selectAll("path").attr("d", path);
---
+++ todo.md
@@ -0,0 +1,24 @@
+# visualDon_earthquakeProject Todolist
+
+## Map
+2d, pleine page
+
+## Page statique
+Html/css
+
+## Timeline
+JS
+
+## Animation
+
+### Bouton play/pause
+
+### Animation timeline
+
+### Zoom/dezoom
+
+### Déplacement
+
+### Magnitude
+
+## Encadrés des informations - optionnel
