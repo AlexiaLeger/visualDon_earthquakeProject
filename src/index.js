898200ae695e4912f208710f2a201aacf0f45d85
Marielle Hänggeli <marielle.hanggeli@gmail.com>
2022-03-29 11:38:00 +0200

    Show map JS

--- src/index.js
+++ src/index.js
@@ -1,94 +1,39 @@
 import * as d3 from 'd3';
 
 //récupération des données
-import dataEarthquakes from '../data/data.json';
-import locationData from '../data/locations.json';
+import data from '../data/data.json';
+import map from '../data/map.json';
+
+
 
 //récupération svg
 
-const width = 960;
-const height = 500;
-const config = {
-    speed: 0.005,
-    verticalTilt: -30,
-    horizontalTilt: 0
-}
-let locations = [];
-const svg = d3.select('svg')
-    .attr('width', width).attr('height', height);
-const markerGroup = svg.append('g');
-const projection = d3.geoOrthographic();
-const initialScale = projection.scale();
+const width = innerWidth;
+const height = innerHeight;
+
+const svg = d3.select("svg").attr("width", width).attr("height", height);
+// Map and projection
+const projection = d3.geoMercator()
+    .center([0, 10])
+    .scale([width / (2 * Math.PI)])
+    .translate([width / 2, height / 2]);
+
+
 const path = d3.geoPath().projection(projection);
-const center = [width / 2, height / 2];
 
-drawGlobe();
-console.log("Globe draw");
-drawGraticule();
-console.log("Graticule draw");
-enableRotation();
-console.log("Rotation enable");
+// Data and color scale
+const myColor = d3.scaleLinear().domain([50, 100])
+    .range(["white", "blue"])
 
-function drawGlobe() {
-    console.log('test1')
-    Promise.all([
-        d3.json('https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json'),
-        d3.json('../data/locations.json')
-    ])       
-        .then((error, worldData, locationData) => {
-            svg.selectAll(".segment")
-                .data(topojson.feature(worldData, worldData.objects.countries).features)
-                .enter().append("path")
-                .attr("class", "segment")
-                .attr("d", path)
-                .style("stroke", "#888")
-                .style("stroke-width", "1px")
-                .style("fill", (d, i) => '#e5e5e5')
-                .style("opacity", ".6");
-            locations = locationData;
-            drawMarkers();
-        });
-}
 
-function drawGraticule() {
-    console.log('test2')
-    const graticule = d3.geoGraticule()
-        .step([10, 10]);
+    // Draw the map
+svg.append("g")
+.selectAll("path")
+.data(map.features)
+.enter()
+.append("path")
+// draw each country
+.attr("d", path)
+// set the color of each country
+.attr("fill", "lightgreen");
 
-    svg.append("path")
-        .datum(graticule)
-        .attr("class", "graticule")
-        .attr("d", path)
-        .style("fill", "#fff")
-        .style("stroke", "#ccc");
-}
-
-function enableRotation() {
-    console.log('test3')
-    d3.timer(function (elapsed) {
-        projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
-        svg.selectAll("path").attr("d", path);
-        drawMarkers();
-    });
-}
-
-function drawMarkers() {
-    const markers = markerGroup.selectAll('circle')
-        .data(locations);
-    markers
-        .enter()
-        .append('circle')
-        .merge(markers)
-        .attr('cx', d => projection([d.longitude, d.latitude])[0])
-        .attr('cy', d => projection([d.longitude, d.latitude])[1])
-        .attr('fill', d => {
-            const coordinate = [d.longitude, d.latitude];
-            gdistance = d3.geoDistance(coordinate, projection.invert(center));
-            return gdistance > 1.57 ? 'none' : 'steelblue';
-        })
-        .attr('r', 7);
-
-    markerGroup.each(function () {
-        this.parentNode.appendChild(this);
-    });
-}
