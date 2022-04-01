74a751eb203c6b5cdaa418bc0c7a4fdb57a9f3f7
Alexia Leger <alexia.leger@heig-vd.ch>
2022-04-01 11:32:44 +0200

    Globe + rotation + début affichage tremblement

--- src/index.js
+++ src/index.js
@@ -13,27 +13,63 @@ const height = innerHeight;
 
 const svg = d3.select("svg").attr("width", width).attr("height", height);
 // Map and projection
-const projection = d3.geoMercator()
+const projection = d3.geoOrthographic()
     .center([0, 10])
-    .scale([width / (2 * Math.PI)])
+    .scale([width / (1.9*Math.PI)])
     .translate([width / 2, height / 2]);
 
 
 const path = d3.geoPath().projection(projection);
 
-// Data and color scale
-const myColor = d3.scaleLinear().domain([50, 100])
-    .range(["white", "blue"])
-
-
-    // Draw the map
+// Draw the map
 svg.append("g")
-.selectAll("path")
-.data(map.features)
-.enter()
-.append("path")
-// draw each country
-.attr("d", path)
-// set the color of each country
-.attr("fill", "white");
+    .selectAll("path")
+    .data(map.features)
+    .enter()
+    .append("path")
+    // draw each country
+    .attr("d", path)
+    // set the color of each country
+    .attr("fill", "white");
 
+//rotation
+enableRotation();
+
+const config = {
+    speed: 0.005,
+    verticalTilt: -30,
+    horizontalTilt: 0
+}
+
+function enableRotation() {
+    d3.timer(function (elapsed) {
+        projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
+        svg.selectAll("path").attr("d", path);
+        drawMarkers();
+    });
+} 
+
+//dessine les tremblements de terre
+drawMarkers();
+const markerGroup = svg.append('g');
+
+function drawMarkers() {
+    const markers = markerGroup.selectAll('circle')
+        .data(data);
+    markers
+        .enter()
+        .append('circle')
+        .merge(markers)
+        .attr('cx', d => projection([d.Longitude, d.Latitude])[0])
+        .attr('cy', d => projection([d.Longitude, d.Latitude])[1])
+        .attr('fill', d => {
+            const coordinate = [d.Longitude, d.Latitude];
+            gdistance = d3.geoDistance(coordinate, projection.invert(center));
+            return gdistance > 1.57 ? 'none' : 'steelblue';
+        })
+        .attr('r', 7);
+
+    markerGroup.each(function () {
+        this.parentNode.appendChild(this);
+    });
+}
